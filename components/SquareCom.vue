a276ab00e0b76a7efe381e288152169f57f61371
*WkMWuglGm9 <k-yamamoto@pscsrv.co.jp>
2022-02-22 19:54:12 +0900

    クイズサイト作成開始

---
+++ components/SquareCom.vue
@@ -0,0 +1,47 @@
+<template>
+    <div
+      class="square1"
+      :style="{background: activeColor}"
+      @click="changeColor">
+      
+    </div>
+</template>
+<script>
+export default {
+  props: {
+    firstColor: {
+      type: String,
+      required: true
+    },
+    secondColor: {
+      type: String,
+      required: true
+    },
+  },
+  data(){
+    return {
+      isActive: true,
+      activeColor: this.firstColor,
+    }
+  },
+  methods: {
+    changeColor() {
+      if(this.activeColor===this.firstColor){
+        this.activeColor=this.secondColor;
+      } else {
+        this.activeColor=this.firstColor;
+      }
+    }
+  }
+
+}
+</script>
+<style>
+.square1 {
+  margin      : auto;
+  margin-bottom: 2em;
+  width       : 200px;
+  height      : 70px;
+}
+ 
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,42 @@
 <template>
-  <Tutorial/>
+  <div class="component-space">
+      <div class="gazo-box">
+        <square-com first-color="red" second-color="green"/><br>
+        <square-com first-color="blue" second-color="yellow"/><br>
+        <square-com first-color="black" second-color="purple"/><br>
+        <square-com first-color="gray" second-color="white"/><br>
+        <square-com first-color="lightgreen" second-color="cyan"/><br>
+      </div>
+
+      <div class="gazo-box">
+        <square-com first-color="orange" second-color="purple"/><br>
+        <square-com first-color="lightblue" second-color="#CEF9DC"/><br>
+        <square-com first-color="cyan" second-color="purple"/><br>
+        <square-com first-color="fuchsia" second-color="red"/><br>
+        <square-com first-color="purple" second-color="olive"/><br>
+      </div>
+    </div>
 </template>
 
 <script>
-export default {}
+import SquareCom from '~/components/SquareCom'
+
+export default {
+  name: 'HomePage',
+  components: {
+    SquareCom,
+  },
+}
 </script>
+
+<style type="text/css">
+  div.gazo-box {
+    float: left;
+    padding-right: 200px;
+    justify-content: center;
+  }
+
+  div.component-space{
+    padding: 300px 500px;
+  }
+</style>
